370a9f27dc48ff520c32c8c7b99e2bc7f6a1e575
george <g.danezis@ucl.ac.uk>
2017-07-10 13:54:40 +0100

    Added a smart meter contract by cloning the bank transfer

---
+++ cspace/examples/smart_meter/smart_meter_checker.py
@@ -0,0 +1,122 @@
+##################################################################################
+# Chainspace Mock
+# smart_meter_checker.py
+#
+##################################################################################
+from json  import loads, dumps
+from flask import Flask, request
+
+
+##################################################################################
+# checker
+##################################################################################
+
+# -------------------------------------------------------------------------------
+# helper
+# -------------------------------------------------------------------------------
+def ccheck(V, msg):
+    if not V:
+        raise Exception(msg)
+
+# -------------------------------------------------------------------------------
+# checker
+# -------------------------------------------------------------------------------      
+def checker_function(T):
+
+    # debug
+    #print(T)
+
+    ##
+    ## create a new bank account
+    ##
+    if T["contractID"] == 1:
+        # check transfer's format
+        ccheck(len(T["inputs"]) == 0, "Expect no inputs")
+        ccheck(len(T["referenceInputs"]) == 0, "Expect no references")
+        ccheck(len(T["parameters"]) == 0, "Expect no parameters")
+        ccheck(len(T["returns"]) == 0, "Expect no returns")
+        ccheck(len(T["outputs"]) == 1, "Expect exactly 1 output")
+
+        # check integrity of the new account
+        newAccount = loads(T[u"outputs"][0])
+        ccheck(newAccount["accountId"] != None, "Malformed account")
+        ccheck(newAccount["amount"] == 10, "Incorrect initial ammount")
+
+
+    ##
+    ## check back transfer
+    ##
+    elif T["contractID"] == 2:
+        # check transfer's format
+        ccheck(len(T["inputs"]) == 2, "Expect exactly 2 inputs")
+        ccheck(len(T["referenceInputs"]) == 0, "Expect no references")
+        ccheck(len(T["parameters"]) == 1, "Expect exactly 1 parameter")
+        ccheck(len(T["returns"]) == 0, "Expect no returns")
+        ccheck(len(T["outputs"]) == 2, "Expect exactly 2 outputs")
+
+        # retrieve inputs
+        from_account     = loads(T[u"inputs"][0])
+        to_account       = loads(T[u"inputs"][1])
+        amount           = loads(T[u"parameters"][0])["amount"]
+        from_account_new = loads(T[u"outputs"][0])
+        to_account_new   = loads(T[u"outputs"][1])
+
+        # check positive amount
+        ccheck(0 < amount, "Transfer should be positive")
+
+        # check sender and receiver account
+        ccheck(from_account["accountId"] == from_account_new["accountId"],  "Old and new account do not match")
+        ccheck(to_account["accountId"]   == to_account_new["accountId"],    "Old and new account do not match")
+
+        # check that the sender has enough fundings
+        ccheck(amount <= from_account["amount"], "No funds available")
+
+        # check inntegrity of the operation
+        ccheck(from_account["amount"] - amount == from_account_new["amount"], "Incorrect new balance")
+        ccheck(to_account["amount"]   + amount == to_account_new["amount"],   "Incorrect new balance")
+
+
+    ##
+    ## wrong method
+    ##
+    else:
+        raise Exception("Wrong method")
+
+    ##
+    ## return
+    ##
+    return {"status": "OK"}
+
+
+
+##################################################################################
+# webapp
+##################################################################################
+# the state of the infrastructure
+app = Flask(__name__)
+
+# -------------------------------------------------------------------------------
+# /bank/transfer
+# checker the correctness of a bank transfer
+# -------------------------------------------------------------------------------
+@app.route("/bank/transfer", methods=["GET", "POST"])
+def check():
+    if request.method == "POST":
+        try:
+            return dumps(checker_function(loads(request.data)))
+        except KeyError as e:
+            return dumps({"status": "ERROR", "message": e.args})
+        except Exception as e:
+            return dumps({"status": "ERROR", "message": e.args})
+    else:
+        return dumps({"status": "ERROR", "message":"Use POST method."})
+
+
+##################################################################################
+# execute
+##################################################################################
+if __name__ == "__main__": 
+    app.run(host="127.0.0.1", port="5001") 
+
+
+##################################################################################
---
+++ cspace/examples/smart_meter/test_smart_meter.py
@@ -0,0 +1,243 @@
+##################################################################################
+# Chainspace Mock
+# test_bank_transfer.py
+#
+# version: 0.0.1
+##################################################################################
+import sys
+sys.path.append('../../')
+from json                   import loads, dumps
+from threading              import Thread
+from cspace_service         import app            as app_cspace
+from smart_meter_checker  import app            as app_checker
+from hashlib                import sha256
+from binascii               import hexlify
+import pytest
+import requests
+
+from multiprocessing import Process
+import time
+
+##################################################################################
+# variables
+##################################################################################
+# URLs
+node_url      =  r"http://127.0.0.1:5000/process"
+checker_url   =  r"http://127.0.0.1:5001/bank/transfer"
+
+# old accounts (before money transfer)
+Sally_account = {"accountId": "Sally", "amount": 10}
+Alice_account = {"accountId": "Alice", "amount": 10}
+
+# new accounts (after money transfer)
+Sally_account_new = {"accountId": "Sally", "amount": 2}
+Alice_account_new = {"accountId": "Alice", "amount": 18}
+
+
+##################################################################################
+# utils
+##################################################################################
+def H(x):
+    return hexlify(sha256(x).digest())
+
+
+##################################################################################
+# run the checker's service
+##################################################################################
+def start_checker(app):
+    try:
+        app.run(host="127.0.0.1", port="5001", threaded=True)
+    except Exception as e:
+        print "The checker is already running:", e
+        assert False
+
+def start_cspace(app):
+    try:
+        app.run(host="127.0.0.1", port="5000", threaded=True)
+    except Exception as e:
+        print "The cspace is already running:", e
+        assert False
+
+
+##################################################################################
+# tests
+##################################################################################
+# -------------------------------------------------------------------------------
+# test 1
+# try to validate a transaction (call the checker) at an hardcoded address
+# -------------------------------------------------------------------------------
+def test_request():
+    # run the checker
+    t = Process(target=start_checker, args=(app_checker,))
+    t.start()
+    time.sleep(0.1)
+
+    # create test vectors
+    createAccount = {
+        "contractID"        : 1,
+        "inputs"            : [],
+        "referenceInputs"   : [],
+        "parameters"        : [],
+        "returns"           : [],
+        "outputs"           : [dumps(Sally_account)],
+        "dependencies"      : []
+    }
+    transfer = {
+        "contractID"        : 2,
+        "inputs"            : [dumps(Sally_account), dumps(Alice_account)],
+        "referenceInputs"   : [],
+        "parameters"        : [dumps({"amount":8})],
+        "returns"           : [],
+        "outputs"           : [dumps(Sally_account_new), dumps(Alice_account_new)],
+        "dependencies"      : []
+    }
+    invalidTransfer = {
+        "contractID"        : 2,
+        "inputs"            : [dumps(Sally_account), dumps(Alice_account)],
+        "referenceInputs"   : [],
+        "parameters"        : [dumps({"amount":100})],
+        "returns"           : [],
+        "outputs"           : [dumps(Sally_account_new), dumps(Alice_account_new)],
+        "dependencies"      : []
+    }
+    malformedTransfer = {
+        "contractID"        : 2,
+        # inputs are missing
+        "referenceInputs"   : [],
+        "parameters"        : [dumps({"amount":8})],
+        "returns"           : [],
+        "outputs"           : [dumps(Sally_account_new), dumps(Alice_account_new)],
+        "dependencies"      : []
+    }
+    invalidOperation = {
+        "contractID"        : 100,
+        "inputs"            : [dumps(Sally_account), dumps(Alice_account)],
+        "referenceInputs"   : [],
+        "parameters"        : [dumps({"amount":1})],
+        "returns"           : [],
+        "outputs"           : [dumps(Sally_account_new), dumps(Alice_account_new)],
+        "dependencies"      : []
+    }
+
+    # execute tests
+    try:
+        # test account creation
+        r = requests.post(checker_url, data = dumps(createAccount))
+        #print(loads(r.text))
+        assert loads(r.text)["status"] == "OK"
+
+        # test a valid transfer
+        r = requests.post(checker_url, data = dumps(transfer))
+        #print(loads(r.text))
+        assert loads(r.text)["status"] == "OK"
+
+        # test a transfer with invalid amount
+        r = requests.post(checker_url, data = dumps(invalidTransfer))
+        #print(loads(r.text))
+        assert loads(r.text)["status"] == "ERROR"
+
+        # test malformed transaction
+        r = requests.post(checker_url, data = dumps(malformedTransfer))
+        #print(loads(r.text))
+        assert loads(r.text)["status"] == "ERROR"
+
+        # test invalid operation
+        r = requests.post(checker_url, data = dumps(invalidOperation))
+        #print(loads(r.text))
+        assert loads(r.text)["status"] == "ERROR"
+
+        # get request
+        r = requests.get(checker_url)
+        #print(loads(r.text))
+        assert loads(r.text)["status"] == "ERROR"
+
+    finally:
+        t.terminate()
+        t.join()
+
+
+# -------------------------------------------------------------------------------
+# test 2
+# final check: simulate a complete transfer & account creation
+# -------------------------------------------------------------------------------
+def test_transaction():
+    # run checker and cspace
+    t1 = Process(target=start_checker, args=(app_checker,))
+    t1.start()
+    t2 = Process(target=start_cspace, args=(app_cspace,))
+    t2.start()
+    time.sleep(0.2)
+
+    try:
+        ##
+        # create Alice's account
+        ##
+        T1 = {
+            "contractID"        : 1,
+            "inputIDs"          : [],
+            "referenceInputIDs" : [],
+            "parameters"        : [],
+            "returns"           : [],
+            "outputs"           : [dumps(Sally_account)],
+            "dependencies"      : []
+        }
+        store1 = []
+        packet1 = {"transaction": T1, "store": store1}
+
+        ##
+        # create Sally's account
+        ##
+        T2 = {
+            "contractID"        : 1,
+            "inputIDs"          : [],
+            "referenceInputIDs" : [],
+            "parameters"        : [],
+            "returns"           : [],
+            "outputs"           : [dumps(Alice_account)],
+            "dependencies"      : []
+        }
+        store2 = []
+        packet2 = {"transaction": T2, "store": store2}
+
+        ##
+        # make the transfer
+        ##
+        ID1 = H( H(dumps(T1)) +"|"+ dumps(Sally_account))
+        ID2 = H( H(dumps(T2)) +"|"+ dumps(Alice_account))
+        T3 = {
+            "contractID"        : 2,
+            "inputIDs"          : [ID1, ID2],
+            "referenceInputIDs" : [],
+            "parameters"        : [dumps({"amount":8})],
+            "returns"           : [],
+            "outputs"           : [dumps(Sally_account_new), dumps(Alice_account_new)],
+            "dependencies"      : [dumps(packet1), dumps(packet2)]
+        }
+        store3 = [
+            {"key": ID1, "value": dumps(Sally_account)},
+            {"key": ID2, "value": dumps(Alice_account)}
+        ]
+        packet3 = {"transaction": T3, "store": store3}
+
+
+        ##
+        # sumbit the transaction to the ledger
+        ##
+        r = requests.post(node_url, data = dumps(packet3))
+        print(loads(r.text))
+        assert loads(r.text)["status"] == "OK"
+
+
+    finally:
+        t1.terminate()
+        t2.terminate()
+        t1.join()
+        t2.join()
+
+        import os
+        import os.path
+        if os.path.isfile("db.json"):
+            os.remove("db.json")
+
+
+##################################################################################
